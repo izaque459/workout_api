5a54fd1e4862f29bc978beb327e768f3db267964
Isaac_Santos <isaacdsantos@hotmail.com>
2024-06-17 19:11:45 -0300

    Criado dicionario do trecho de codigo

--- workout_api/main.py
+++ workout_api/main.py
@@ -4,7 +4,8 @@ from workout_api.routers import api_router
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
 
-if __name__ == 'main':
+"""if __name__ == 'main':
     import uvicorn
     
     uvicorn.run('main.app',host='0.0.0.0', port=8000,log_level='info',reload=True)
+"""
